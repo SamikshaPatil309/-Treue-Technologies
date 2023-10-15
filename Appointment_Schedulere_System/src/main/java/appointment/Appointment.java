1e583d3ce27118c518c35e3bcea42beeab880db9
SamikshaPatil309 <134995041+SamikshaPatil309@users.noreply.github.com>
2023-10-15 16:47:51 +0530

    Add files via upload

---
+++ Appointment_Schedulere_System/src/main/java/appointment/Appointment.java
@@ -0,0 +1,66 @@
+package appointment;
+
+import java.time.LocalDateTime;
+
+public class Appointment {
+	
+	private LocalDateTime startTime;
+	
+
+	public Appointment(LocalDateTime startTime, LocalDateTime endTime, String serviceProvider, User user) {
+		super();
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.serviceProvider = serviceProvider;
+		this.user = user;
+	}
+
+	private LocalDateTime endTime;
+    private String serviceProvider;
+    private User user;
+    
+  
+
+	public LocalDateTime getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(LocalDateTime startTime) {
+		this.startTime = startTime;
+	}
+
+	public LocalDateTime getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(LocalDateTime endTime) {
+		this.endTime = endTime;
+	}
+
+	public String getServiceProvider() {
+		return serviceProvider;
+	}
+
+	public void setServiceProvider(String serviceProvider) {
+		this.serviceProvider = serviceProvider;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public String toString() {
+		return "Appointment [startTime=" + startTime + ", endTime=" + endTime + ", serviceProvider=" + serviceProvider
+				+ ", user=" + user + "]";
+	}
+	
+
+	
+    
+
+}
